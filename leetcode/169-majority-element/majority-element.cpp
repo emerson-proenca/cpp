df5a90f084a75857cd0c585b7e0ed5310a5e7af9
Emerson Proença <79228789+emerson-proenca@users.noreply.github.com>
2026-01-26 18:59:10 -0300

    Time: 0 ms (100.00%) | Memory: 28.2 MB (62.46%) - LeetSync

--- leetcode/169-majority-element/majority-element.cpp
+++ leetcode/169-majority-element/majority-element.cpp
@@ -4,15 +4,14 @@ public:
         int c = 0;
         int n = nums[0];
 
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); ++i) {
             if (c == 0) {
                 n = nums[i];
             } 
             if (nums[i] != n) {
-                c--;
-            }
-            if (nums[i] == n) {
-                c++;
+                --c;
+            } else {
+                ++c;
             }
         }
         return n;
