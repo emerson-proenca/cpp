682eeda83acb03f92ba278be4bbda25e9994cbc2
Emerson Proença <79228789+emerson-proenca@users.noreply.github.com>
2026-01-26 18:58:03 -0300

    Time: 0 ms (100.00%) | Memory: 28.3 MB (27.29%) - LeetSync

---
+++ leetcode/169-majority-element/majority-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int c = 0;
+        int n = nums[0];
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (c == 0) {
+                n = nums[i];
+            } 
+            if (nums[i] != n) {
+                c--;
+            }
+            if (nums[i] == n) {
+                c++;
+            }
+        }
+        return n;
+    }
+};
