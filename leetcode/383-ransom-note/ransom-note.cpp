da9a29302b6b974722b9463834c8ad5fd6484ae8
Emerson Proença <79228789+emerson-proenca@users.noreply.github.com>
2026-01-28 10:27:35 -0300

    Time: 4 ms (43.67%) | Memory: 12 MB (11.23%) - LeetSync

--- leetcode/383-ransom-note/ransom-note.cpp
+++ leetcode/383-ransom-note/ransom-note.cpp
@@ -2,24 +2,17 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char, int> mp;
-        for (int i = 0; i < magazine.size(); ++i) {
-            if (mp.find(magazine[i]) == mp.end()) {
-                mp[magazine[i]] = 1;
-            } else {
-                mp[magazine[i]] += 1;
-            }
-            cout << mp[magazine[i]] << " " << endl;
+        for (char c : magazine) {
+            mp[c]++;
         }
 
-        for (int j = 0; j < ransomNote.size(); ++j) {
-            cout << ransomNote[j] << " " << endl;
-            if (mp.find(ransomNote[j]) != mp.end() && mp[ransomNote[j]] > 0) {
-                mp[ransomNote[j]] -= 1;
-            } else {
-                    return false;
-                }
+        for (char c : ransomNote) {
+            if (mp[c] <= 0) {
+                return false;
             }
-    
+            mp[c]--;
+        }
+
         return true;
     }
 };
