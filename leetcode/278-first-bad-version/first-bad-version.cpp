e8471801aac6e22d2ae183e6246b169bb551e961
Emerson Proença <79228789+emerson-proenca@users.noreply.github.com>
2026-01-30 01:03:30 -0300

    Time: 2 ms (56.12%) | Memory: 7.9 MB (40.93%) - LeetSync

--- leetcode/278-first-bad-version/first-bad-version.cpp
+++ leetcode/278-first-bad-version/first-bad-version.cpp
@@ -5,18 +5,16 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int low = 0;
-        int high = n;
-        int version = n;
+        int high = n - 1;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (isBadVersion(mid)) {
                 high = mid - 1;
-                version = mid;
             } else {
                 low = mid + 1;
             }
         }
-        return version;
+        return low;
     }
 };
