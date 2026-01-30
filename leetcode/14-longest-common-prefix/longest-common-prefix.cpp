94cb125dd9f46d841ae78be9fc255e5e09d45f87
Emerson Proença <79228789+emerson-proenca@users.noreply.github.com>
2026-01-29 22:59:49 -0300

    Time: 0 ms (100.00%) | Memory: 11.9 MB (63.39%) - LeetSync

---
+++ leetcode/14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string str = strs[0];
+
+        for (auto word : strs) {
+            if (word.length() < str.length()) {
+                str.erase(word.length());
+            }
+            for (int i = 0; i < word.length(); ++i) {
+                if (str[i] != word[i]) {
+                    str.erase(i);
+                    break;
+                }
+            }
+        }
+        if (str.size() < 1) {
+            return "";
+        }
+        return str;
+    }
+};
