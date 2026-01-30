19081f5703a42bf2221a7e69dda6026dddcf227d
Emerson Proença <79228789+emerson-proenca@users.noreply.github.com>
2026-01-29 23:41:55 -0300

    Time: 0 ms (100.00%) | Memory: 12.4 MB (8.83%) - LeetSync

--- leetcode/14-longest-common-prefix/longest-common-prefix.cpp
+++ leetcode/14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,19 +1,47 @@
+struct TrieNode {
+    TrieNode* children[26] = {nullptr};
+    int childCount = 0;
+    bool isEndOfWord = false;
+
+    void insert(string word) {
+        TrieNode* curr = this;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (!curr->children[idx]) {
+                curr->children[idx] = new TrieNode();
+                curr->childCount++;
+            }
+            curr = curr->children[idx];
+        }
+        curr->isEndOfWord = true;
+    }
+};
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string str = strs[0];
+        if (strs.empty()) return "";
+        if (strs.size() == 1) return strs[0];
 
-        for (const string& word : strs) {
-            if (word.length() < str.length()) {
-                str.erase(word.length());
-            }
-            for (int i = 0; i < word.length(); ++i) {
-                if (str[i] != word[i]) {
-                    str.erase(i);
-                    break;
+        TrieNode* root = new TrieNode();
+        for (const string& s : strs) {
+            if (s.empty()) return ""; // If any string is empty, prefix is ""
+            root->insert(s);
+        }
+
+        string prefix = "";
+        TrieNode* curr = root;
+        
+        // Traverse until we hit a branch or the end of a word
+        while (curr->childCount == 1 && !curr->isEndOfWord) {
+            for (int i = 0; i < 26; i++) {
+                if (curr->children[i]) {
+                    prefix += (char)('a' + i);
+                    curr = curr->children[i];
+                    break; 
                 }
             }
         }
-        return str;
+        return prefix;
     }
 };
